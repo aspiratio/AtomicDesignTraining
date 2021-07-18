78d89f3957efad784c2cba96258076b8dbb4c17d
aspiratio <80562226+aspiratio@users.noreply.github.com>
2021-07-18 23:01:33 +0900

    ContextとRecoilでグローバルState実装

--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -1,12 +1,25 @@
 import styled from "styled-components";
 import { SecondaryButton } from "../atoms/button/SecondaryButton";
 import { useHistory } from "react-router-dom";
+// import { useContext } from "react";
+// import { UserContext } from "../../providers/UserProvider";
+// 値は使わずsetStateだけ使う時はuseSetRecoilState
+import { useSetRecoilState } from "recoil";
+import { userState } from "../../store/userState";
 
 export const Top = () => {
   const history = useHistory();
+  // const { setUserInfo } = useContext(UserContext);
+  const setUserInfo = useSetRecoilState(userState);
 
-  const onClickAdmin = () => history.push("/users");
-  const onClickGeneral = () => history.push("/users");
+  const onClickAdmin = () => {
+    setUserInfo({ isAdmin: true });
+    history.push("/users");
+  };
+  const onClickGeneral = () => {
+    setUserInfo({ isAdmin: false });
+    history.push("/users");
+  };
 
   return (
     <SContainer>
